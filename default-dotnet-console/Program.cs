117bc0331b1c18a69c7153413944c779d32dc032
Leeyoungkyu <amdusias209@gmail.com>
2024-02-06 07:27:15 +0000

    ✨ Implement main

--- default-dotnet-console/Program.cs
+++ default-dotnet-console/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class MainClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine("Hello, World!");
+    }
+}
